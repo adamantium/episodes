b6245aa35efd0ff856b366dd72ee1679bed44a46
Changhoon Lee <adamantium.kr@gmail.com>
2011-12-29 06:34:38 +0900

    Changed to support latest epguides.com. Implemented Timezone converter.

--- server/collector.py
+++ server/collector.py
@@ -1,7 +1,9 @@
 from BeautifulSoup import BeautifulSoup
 from pymongo import Connection
 from datetime import datetime
+from datetime import timedelta
 from utils import TimeSlot
+from utils import DatetimeConverter
 import logging
 import re
 import urllib2
@@ -19,12 +21,14 @@ class Collector(object):
         # Connect to DB
         self.db = Connection('localhost', 27017)[dbname]
         self.collection_prefix = collection_prefix
-        self.grid = self.db[collection_prefix + '_grid']
+        self.shows = self.db[collection_prefix + '_shows']
+        self.timetable = self.db[collection_prefix + '_timetable']
+        self.next = self.db[collection_prefix + '_next']
         self.index = self.db[collection_prefix + '_index']
         self.timestamps = self.db[collection_prefix + '_timestamps']
 
         # Set logger
-        self.logger = logging.getLogger("EPISODES")
+        self.logger = logging.getLogger("EPISODES COLLECTOR")
         self.logger.setLevel(logging.DEBUG)
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
@@ -32,7 +36,10 @@ class Collector(object):
         ch.setFormatter(formatter)
         self.logger.addHandler(ch)
 
-        # Temporary Store
+        # Set Datetime Converter
+        self.dc = DatetimeConverter('est', date_format='%d/%b/%y')
+
+        # Temporary store
         self.temp_index = [];
     
     def element_is_tag(self, elem):
@@ -48,12 +55,6 @@ class Collector(object):
     
     def update_timestamp(self, target, update_time):
         self.timestamps.update({'target': target}, {'$set': {'timestamp': update_time}}, upsert=True)
-
-    def update_one(self):
-        raise NotImplementedError
-    
-    def update_grid(self):
-        raise NotImplementedError
     
     def update(self):
         raise NotImplementedError
@@ -78,16 +79,11 @@ class EpgCollector(Collector):
             if tvrage_show_summary:
                 return 'http://epguides.com/common/exportToCSV.asp?rage=' + tvrage_show_summary[0]['href'][31:]
     
-    def update_one(self, show_ccn):
-        show = self.grid.find_one({'ccn': show_ccn})
-
-        # FIXME: Add exception handler instead of simple print string
-        if not show:
-            print "Show must be inserted first"
-            return None
-
+    def update_one(self, show_ccn, link, start_time, duration):
+        duration = timedelta(minutes = duration)
+        
         # Load detail show page and check if the latest data is updated    
-        page = urllib2.urlopen(show['link'])
+        page = urllib2.urlopen(link)
         soup = BeautifulSoup(page)
         update_time = datetime.strptime(soup.em.string.replace('-', ''), DATE_FORMAT_B)
         if not self.is_latest(show_ccn, update_time):
@@ -104,26 +100,91 @@ class EpgCollector(Collector):
             csv_page = urllib2.urlopen(csv_link)
 
             # Convert CSV string to list of episode
-            csv_string = BeautifulSoup(csv_page).textarea.string.replace('\r\n', '')
+            csv_string = BeautifulSoup(csv_page).pre.string.replace('\r\n', '')
             csv_ep_list = csv_string.split('\n')[1:]
-            ep_list = [csv_ep.split(',') for csv_ep in csv_ep_list]
+            ep_list = []
+
+            # Examine each episode to classify
+            for csv_ep in csv_ep_list:
+                ep_info_split = csv_ep.split(',')
+                ep_info = []
+                for ep_info_item in ep_info_split:
+                    if len(ep_info_item) > 0:
+                        if (ep_info_item[0] == '"') and (ep_info_item[-1] == '"'):
+                            ep_info_item = ep_info_item[1:-1]
+                    ep_info.append(ep_info_item)
+                idx, season, num, pcode, airdate = ep_info[:5]
+                ep_title = ','.join(ep_info[5:-2])
+                if ep_info[-1] == 'n':
+                    is_special = False
+                else:
+                    is_special = True
+
+                if idx:
+                    if idx.isdigit():
+                        idx = int(idx)
+                    else:
+                        self.logger.error('Index "%s" must be a number - %s for %s' % (idx, str(csv_ep), title))
+                else:
+                    if is_special:
+                        idx = 0
+                    else:
+                        self.logger.error('Only special episodes can have a empty index - %s for %s' % (str(csv_ep), title))
+                        idx = -1
+                
+                if season.isdigit():
+                    season = int(season)
+                else:
+                    self.logger.error('Season "%s" must be a number - %s for %s' % (season, str(csv_ep), title)) 
+                    season = -1
+
+                if num.isdigit():
+                    num = int(num)
+                else:
+                    if is_special != True:
+                        self.logger.error('Episode number "%s" must be a number - %s for %s' % (num, str(csv_ep), title))
+                        num = -1
+                    else:
+                        num = 0
+
+                airdate = str(airdate.replace('"', ''))
+                date_fmt = re.compile('\d\d/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/\d\d')
+                if airdate == 'UNKNOWN':
+                    air_dt_tuple = [9999, 12, 31, 23, 59]
+                elif date_fmt.match(airdate):
+                    # Combine timeslot and date to make datetime object and convert timezone to kst
+                    kst_air_dt = self.dc.convert_tz('kst', (str(airdate), start_time))
+                    kst_end_dt = kst_air_dt + duration
+                    air_dt_tuple = self.dc.datetime_to_tuple(kst_air_dt)
+                    
+                    ep_ccn = show_ccn + ep_title + str(idx)
+                    if kst_air_dt > self.now:
+                        status = 'yet'
+                        self.next.update({'ccn': ep_ccn}, {'$set': {'title': title, 'ad': air_dt_tuple}}) 
+                    elif kst_end_dt < self.now:
+                        status = 'aired'
+                    else:
+                        status = 'airing'
+                elif airdate == 'UNAIRED':
+                    air_dt_tuple = [0000, 1, 1, 0, 0]
+                else:
+                    air_dt_tuple = [-1, -1, -1, -1, -1]
+
+                # Build up episode information list. 
+                ep = [idx, season, num, pcode, air_dt_tuple, ep_title, is_special]
+                ep_list.append(ep)
 
             # Update detail information about the show
-            self.grid.update({'ccn': show_ccn}, {'$set': {'episodes': ep_list}})
+            self.shows.update({'ccn': show_ccn}, {'$set': {'episodes': ep_list}})
             self.update_timestamp(show_ccn, update_time)
 
             # Upsert show into index
             self.index.update({'ccn': show_ccn}, {'$set': {'title': title, 'imdb_m': imdb_link}}, upsert=True)
             self.temp_index.append([show_ccn, title, imdb_link])
-            self.logger.info('Episodes Updated for ' + show_ccn + ' titled with ' + title)
+            self.logger.info('Episodes Updated for ' + show_ccn + ' titled with ' + title)  
         else:
+            # Data is the latest, so no need to update
             self.logger.info('Episodes for ' + show_ccn + ' is latest.')
-
-    def upsert_one(self, show_info, update=True):
-        self.grid.update({'ccn': show_info[0]}, {'$set': {'link': show_info[1], 'channel': show_info[2], 'dow': show_info[3], 'stime': show_info[4], 'etime': show_info[5], 's': show_info[6], 'r': show_info[7]}}, upsert=True)
-        self.logger.debug('Grid Upserted - ' + str(show_info))
-        if update:
-            self.update_one(show_info[0])
 
     def update_index(self):
         if not self.temp_index:
@@ -131,8 +192,8 @@ class EpgCollector(Collector):
             pass
         self.index.update({'ccn': 'totalindexlist'}, {'$set': {'list': self.temp_index}}, upsert=True)
 
-    def update_grid(self):
-        self.logger.info('START UPDATING GRID')
+    def update(self):
+        self.logger.info('Start updating grid')
         # Load the grid page from www.epguides.com
         page = urllib2.urlopen('http://epguides.com/grid/')
         page_data = page.read()
@@ -148,6 +209,9 @@ class EpgCollector(Collector):
         # Get the latest update time of the loaded grid page
         update_time = datetime.strptime(soup.em.string, DATE_FORMAT_A)
         self.logger.info('FINISH LOADING GRID PAGE')
+
+        # Set current datetime for determining aired or not-yet
+        self.now = self.dc.set_tz('kst', datetime.now())
 
         if not self.is_latest('grid', update_time):
             # Data is not latest, so update the grid data. Prepare the table list witch contains valid 
@@ -188,14 +252,18 @@ class EpgCollector(Collector):
 
                     # Start digging into slots.
                     for td in td_list[1:]:
-                        num_of_slots = td['colspan']
+                        num_of_slots = int(td['colspan'])
                         start_time, end_time = timeslot.next(int(num_of_slots))
                         if self.is_valid_slot(td):
                             # Gather information about the show in current slot.                         
                             link = 'http://www.epguides.com' + td.a['href'][2:]
-                            camelcase_name = link[24:-1]
+                            ccn = link[24:-1] # Show identifier in camelcase
                             status = 'air'
-                            self.upsert_one([camelcase_name, link, channel_name, day_of_week, start_time, end_time, status, ''])
+                            duration = num_of_slots * 30
+                            # Upsert current show. This job perform update of the list of episodes automatically.
+                            self.shows.update({'ccn': ccn}, {'$set': {'l': link, 'c': channel_name, 'w': day_of_week, 't': start_time, 'd': duration, 's': status}}, upsert=True)
+                            self.logger.debug('Show Upserted - ' + ccn)
+                            self.update_one(ccn, link, start_time, duration)
                 day_of_week_idx += 1
 
             # Load shows on hiatus
@@ -204,22 +272,27 @@ class EpgCollector(Collector):
             li_list = table.findAll('li')
             for li in li_list:
                 link = 'http://www.epguides.com' + li.a['href'][2:]
-                camelcase_name = link[24:-1]
+                ccn = link[24:-1]
                 return_date = li.span.string[9:]
                 channel_name = 'UNKNOWN'
                 day_of_week = 'UNKNOWN'
-                start_time = [25, 61]
-                end_time = [25, 61]
+                start_time = [0, 0]
+                end_time = [0, 0]
+                duration = 0
                 status = 'hiatus'
-                self.upsert_one([camelcase_name, link, channel_name, day_of_week, start_time, end_time, status, return_date])
+                self.shows.update({'ccn': ccn}, {'$set': {'l': link, 'c': channel_name, 'w': day_of_week, 't': start_time, 'd': duration, 's': status}}, upsert=True)
+                self.logger.debug('Show Upserted - ' + ccn)
+                self.update_one(ccn, link, start_time, duration)
             
             # TODO: Load shows that are cancelled/ended
             self.update_timestamp('grid', update_time)
         else:
             self.logger.info('DATA IS LATEST [' + update_time.isoformat() + ']. Start updating episodes.')
             # If grid data is latest, update each show detail iterating grid.
-            for show in self.grid.find():
+            for show in self.shows.find():
                 self.update_one(show['ccn'])
+        
+        #
 
 class Usage(Exception):
     def __init__(self, msg):
@@ -235,8 +308,7 @@ def main(argv=None):
             Connection('localhost', 27017).drop_database('episode')
         if 't' in argv[1]:
             print 'TEST START'
-    epgc.update_grid()
-    epgc.update_index()
+    epgc.update()
 
 if __name__ == "__main__":
     sys.exit(main())
--- server/utils.py
+++ server/utils.py
@@ -1,4 +1,13 @@
 import copy
+import datetime
+import pytz
+from types import *
+
+class FatalError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
 
 class TimeSlot(object):
     """Represent, manipulate time-slot"""
@@ -22,3 +31,115 @@ class TimeSlot(object):
         start = copy.deepcopy(self.current)
         self.current = copy.deepcopy(end)
         return start, end
+
+class DatetimeConverter(object):
+    """docstring for DatetimeConverter"""
+    def __init__(self, default_timezone, date_format = None, time_format = None):
+        super(DatetimeConverter, self).__init__()
+
+        # Timezone presets
+        self.tz_utc = pytz.utc
+        self.tz_eastern = pytz.timezone('US/Eastern')
+        self.tz_central = pytz.timezone('US/Central')
+        self.tz_seoul = pytz.timezone('Asia/Seoul')
+        self.tz_list = [tz.lower() for tz in pytz.all_timezones]
+
+        # Set default_timezone.
+        # TODO: Need more adjustment to support more various timezone keywords.
+        dtz = default_timezone.lower()
+        self.dtz = self.parse_tz(dtz)
+        
+        # Set default formats
+        self.date_format = date_format
+        self.time_format = time_format
+    
+    def set_tz(self, timezone, source_dt_tuple):
+        dt = self.parse_dt_tuple(source_dt_tuple)
+        timezone = self.parse_tz(timezone)
+        return timezone.localize(dt)
+
+    def convert_tz(self, target_timezone, source_dt_tuple, source_timezone = None):
+        source_dt = self.parse_dt_tuple(source_dt_tuple)
+        if not source_timezone:
+            source_timezone = self.dtz
+        source_timezone = self.parse_tz(source_timezone)
+        target_timezone = self.parse_tz(target_timezone)
+        loc_dt = source_timezone.localize(source_dt)
+        target_dt = loc_dt.astimezone(target_timezone)
+        return target_dt
+
+    def datetime_to_tuple(self, dt):
+        return (dt.year, dt.month, dt.day, dt.hour, dt.minute)
+
+    def convert_fmt(self, source_dt_tuple, target):
+        # TODO: Implement a format converter
+        pass
+
+    def parse_tz(self, timezone):
+        # Pass through if timezone is tzinfo object
+        # else, find a valid timezone matched to input string 
+        if isinstance(timezone, datetime.tzinfo):
+            return timezone
+        elif isinstance(timezone, str):
+            ltz = timezone.lower()
+            if (ltz == 'utc') or (ltz == 'gmt'):
+                return pytz.utc
+            elif (ltz == 'edt') or (ltz == 'est'):
+                return self.tz_eastern
+            elif ltz == 'cst':
+                return self.tz_central
+            elif ltz == 'kst':
+                return self.tz_seoul
+            elif ltz in self.tz_list:
+                return pytz.timezone(ltz)
+            raise pytz.exceptions.UnknownTimeZoneError(ltz)
+        raise FatalError('Cannot parse timezone')
+
+    def parse_dt_tuple(self, dt_tuple):
+        if isinstance(dt_tuple, datetime.datetime):
+            return dt_tuple
+        assert isinstance(dt_tuple, tuple), "%s must be instance of tuple" % (dt_tuple,)
+        length = len(dt_tuple)
+        if length == 1:
+            dt = dt_tuple[0]
+            assert isinstance(dt, datetime.datetime), "%s must be instance of datetime" % (dt,)
+            return dt
+        elif length == 2:
+            d, t = dt_tuple
+            if isinstance(d, str):
+                date = datetime.datetime.strptime(d, self.date_format).date()
+            else:
+                #assert (isinstance(d, list) or isinstance(d, tuple)) and len(d) == 3, "%s must be instance of list or tuple with length 3" % (d,)
+                date = datetime.date(d[0], d[1], d[2])
+            if isinstance(t, str):
+                time = datetime.datetime.strptime(t, self.time_format).time()
+            else: 
+                #assert (isinstance(t, list) or isinstance(t, tuple)) and len(t) == 2, "%s must be instance of list or tuple with length 2" % (d,)
+                time = datetime.time(t[0], t[1])
+            return datetime.datetime.combine(date, time)
+        elif length == 3:
+            a, b, c = dt_tuple
+            if isinstance(a, str):
+                #assert isinstance(b, str) and (isinstance(c, list) or isinstance(c, tuple))
+                date = datetime.datetime.strptime(d, b).date()
+                time = datetime.time(c[0], c[1])
+            else:
+                #assert (isinstance(a, list) or isinstance(a, tuple)) and isinstance(b, str) and isinstance(c, str)
+                date = datetime.date(a[0], a[1], a[2])
+                time = datetime.datetime.strptime(b, c).time()
+            return datetime.datetime.combine(date, time)
+        elif length == 4:
+            d, df, t, tf = dt_tuple
+            # TODO: add asserts for type checking.
+            if df == 'default':
+                df = self.date_format
+            if tf == 'default':
+                tf = self.time_format
+            date = datetime.datetime.strptime(d, df).date()
+            time = datetime.datetime.strptime(t, tf).time()
+            return datetime.datetime.combine(date, time)
+        raise FatalError('Cannot parse datetime tuple - %s.' % (dt_tuple,))
+    
+    
+
+
